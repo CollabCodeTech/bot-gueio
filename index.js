0886dce3ad6a1e08fb5e0a3f4301e85c675a8bbe
Marco Bruno <marco.bruno.br@gmail.com>
2019-03-11 02:14:07 +0000

    Configurações globais do bot coloquei no .env

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 require("dotenv").config();
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const { TOKEN } = process.env;
+const { TOKEN, WELCOME_CHANNEL, ENTER_CHANNEL } = process.env;
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
-
 client.on("message", msg => {
   if (msg.content === "!ping") msg.reply(`Pong! Latência | ${client.ping}ms`);
 });
 
+client.on("messageReactionAdd", (msg, user) => {
+  if (
+    msg.message.channel.name === ENTER_CHANNEL &&
+    msg.emoji.id === "554458703034712080"
+  ) {
+    msg.message.member.addRole("554409793859747873");
+  }
+});
 
 client.on("guildMemberAdd", member => {
   const channel = member.guild.channels.find(
-    channel => channel.name === "💨boas-vindas💨"
+    channel => channel.name === CHANNEL_WELCOME
   );
 
   if (!channel) return;

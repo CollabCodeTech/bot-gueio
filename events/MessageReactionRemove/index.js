759899a57fecf924e0616436624fc0544fb65b68
Marco Bruno <marco.bruno.br@gmail.com>
2019-03-18 07:37:12 +0000

    Adicionado a funcionalidade de remover uma role a partir de uma reaction

--- events/MessageReactionRemove/index.js
+++ events/MessageReactionRemove/index.js
@@ -3,17 +3,17 @@
 const { ENTER_CHANNEL } = process.env;
 const categories = require("./config");
 
-const enterChannel = (reaction, user) => {
+const quitChannel = (reaction, user) => {
   if (reaction.message.channel.name === ENTER_CHANNEL) {
     const category = categories[reaction.emoji.name];
 
     if (category) {
       const member = reaction.emoji.guild.members.get(user.id);
-      member.addRole(category.role.id);
+      member.removeRole(category.role.id);
     }
   }
 };
 
 module.exports = {
-  enterChannel
+  quitChannel
 };

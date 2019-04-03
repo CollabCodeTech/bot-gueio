ecbbb2891b6113cbce690f498e45bf1a76f0c4ae
Marco Bruno <marco.bruno.br@gmail.com>
2019-04-03 08:14:35 -0300

    Comecei a estruturar como fica as respostas no banco de dados, ainda sem banco mas com a estrutura se definindo

--- events/Message/signup.js
+++ events/Message/signup.js
@@ -1,8 +1,40 @@
 "use strict";
 
+const dbName = [
+  "marcobrunobr"
+];
+
+const dbQuestions = [
+  "Você quer que seu currículo publico? Por favor, responda com **Sim** ou **Não**!"
+];
+
+const exampleDb = {
+  user: "Nome#1234",
+  survey: [
+    {
+      question: "",
+      response: ""
+    }
+  ],
+  profile: {
+    name: "marcobrunobr",
+    url: "http://collabcode.training/marcobrunobr"
+  }
+};
+
+const db = {}
+
 const signup = msg => {
-  msg.author.send(`Depois que você responder todas as peguntas será gerado um currículo online para você usar nas entrevistas! Bora pra primeira pergunta...
-Você quer que seu currículo privado? Por favor, responda com **Sim** ou **Não**!`);
+  const {author} = msg;
+
+  db.user = `${author.username}#${author.discriminator}`
+
+  author.send(`Depois que você responder todas as peguntas será gerado um currículo online para você usar nas entrevistas! Bora pra primeira pergunta...
+${dbQuestions[0]}`);
+
+  db.survey.push({
+    question: dbQuestions[0]
+  })
 
 
   return false;

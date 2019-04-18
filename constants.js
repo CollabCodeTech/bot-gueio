799233ad026a39a134fb45ccf9a800015180ab58
Marco Bruno <marco.bruno.br@gmail.com>
2019-04-18 01:42:09 -0300

    Colocado o comando do curso em primeiro lugar para ter um destaque :-)

--- constants.js
+++ constants.js
@@ -1,22 +1,28 @@
 const commands = [{
-    'command': 'ping',
-    'response': 'Pong! Latência | ${client.ping} msg'
-}, {
-    'command': 'twitter',
-    'response': 'https://twitter.com/collabcodetech'
-}, {
-    'command': 'twitch',
-    'response': 'http://twitch.tv/marcobrunobr'
-},{
-    'command': 'youtube',
-    'response': 'https://www.youtube.com/collabcode'
-},{
     'command': 'curso',
     'response': 'Para saber mais sobre o curso como, **ementa**, **próxima turma** ou qualquer outra parada, entre no site: http://dofrontaoend.collabcode.training'
-},{
+},
+{
+    'command': 'ping',
+    'response': 'Pong! Latência | ${client.ping} msg'
+}, 
+{
+    'command': 'twitter',
+    'response': 'https://twitter.com/collabcodetech'
+}, 
+{
+    'command': 'twitch',
+    'response': 'http://twitch.tv/marcobrunobr'
+},
+{
+    'command': 'youtube',
+    'response': 'https://www.youtube.com/collabcode'
+},
+{
     'command': 'facebook',
     'response': 'https://www.facebook.com/collabcode'
-},{
+},
+{
     'command': 'comandos',
     'showAllCommands': true
 }]

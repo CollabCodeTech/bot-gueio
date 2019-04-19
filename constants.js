bc34a268e676150f01ae1de2c822f79ca7c5b3b1
Marco Bruno <marco.bruno.br@gmail.com>
2019-04-18 22:30:20 -0300

    Criamos o comando de cadastro

--- constants.js
+++ constants.js
@@ -1,30 +1,34 @@
 const commands = [{
-    'command': 'curso',
-    'response': 'Para saber mais sobre o curso como, **ementa**, **próxima turma** ou qualquer outra parada, entre no site: http://dofrontaoend.collabcode.training'
+    "command": "cadastro",
+    "response": "Bora se cadastrar!!!"
 },
 {
-    'command': 'ping',
-    'response': 'Pong! Latência | ${client.ping} msg'
+    "command": "curso",
+    "response": "Para saber mais sobre o curso como, **ementa**, **próxima turma** ou qualquer outra parada, entre no site: http://dofrontaoend.collabcode.training"
+},
+{
+    "command": "ping",
+    "response": "Pong! Latência | ${client.ping} msg"
 }, 
 {
-    'command': 'twitter',
-    'response': 'https://twitter.com/collabcodetech'
+    "command": "twitter",
+    "response": "https://twitter.com/collabcodetech"
 }, 
 {
-    'command': 'twitch',
-    'response': 'http://twitch.tv/marcobrunobr'
+    "command": "twitch",
+    "response": "http://twitch.tv/marcobrunobr"
 },
 {
-    'command': 'youtube',
-    'response': 'https://www.youtube.com/collabcode'
+    "command": "youtube",
+    "response": "https://www.youtube.com/collabcode"
 },
 {
-    'command': 'facebook',
-    'response': 'https://www.facebook.com/collabcode'
+    "command": "facebook",
+    "response": "https://www.facebook.com/collabcode"
 },
 {
-    'command': 'comandos',
-    'showAllCommands': true
+    "command": "comandos",
+    "showAllCommands": true
 }]
 
 module.exports = {

d297b075f13d44963e89ecba87a1b1471f916505
Marco Bruno <marco.bruno.br@gmail.com>
2019-07-05 23:10:58 +0100

    Semana do front ao end terceira turma

--- constants.js
+++ constants.js
@@ -1,32 +1,35 @@
-const commands = [{
-    'command': 'curso',
-    'response': 'Para saber mais sobre o curso como, **ementa**, **próxima turma** ou qualquer outra parada, entre no site: http://dofrontaoend.collabcode.training'
-},
-{
-    'command': 'ping',
-    'response': 'Pong! Latência | ${client.ping} msg'
-}, 
-{
-    'command': 'twitter',
-    'response': 'https://twitter.com/collabcodetech'
-}, 
-{
-    'command': 'twitch',
-    'response': 'http://twitch.tv/marcobrunobr'
-},
-{
-    'command': 'youtube',
-    'response': 'https://www.youtube.com/collabcode'
-},
-{
-    'command': 'facebook',
-    'response': 'https://www.facebook.com/collabcode'
-},
-{
-    'command': 'comandos',
-    'showAllCommands': true
-}]
+const commands = [
+  {
+    command: "curso",
+    response:
+      "Inscrição para **Semana Do Front ao End** abertas, essa é uma oportunidade free e ao vivo para você conhecer o curso, faça seu cadastro: http://dofrontaoend.collabcode.training"
+  },
+  {
+    command: "ping",
+    response: "Pong! Latência | ${client.ping} msg"
+  },
+  {
+    command: "twitter",
+    response: "https://twitter.com/collabcodetech"
+  },
+  {
+    command: "twitch",
+    response: "http://twitch.tv/marcobrunobr"
+  },
+  {
+    command: "youtube",
+    response: "https://www.youtube.com/collabcode"
+  },
+  {
+    command: "facebook",
+    response: "https://www.facebook.com/collabcode"
+  },
+  {
+    command: "comandos",
+    showAllCommands: true
+  }
+];
 
 module.exports = {
-    commands: commands
-}
+  commands: commands
+};

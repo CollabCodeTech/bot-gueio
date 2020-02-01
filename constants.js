74fe937b5e930d52b5430a5a5bde750213dcd97a
Marco Bruno <marco.bruno.br@gmail.com>
2020-02-01 10:35:42 +0100

    :sparkles: Add command telegram

--- constants.js
+++ constants.js
@@ -1,12 +1,15 @@
 const commands = [
   {
-    command: "curso",
-    response:
-      "Se inscreva na **Semana Do Front ao End** para receber o **eBook** e **layout do jogo** que faremos no mini-curos **ao vivo** e de **graça** de HTML, CSS e JavaScript nos dias **9, 10, 11, 12 e 13 de Dezembro**. Para se inscrever só entrar no site: https://dofrontaoend.collabcode.training/"
-  },
-  {
     command: "ping",
     response: "Pong! Latência | ${client.ping} msg"
+  },
+  {
+    command: "invite", 
+    response: "http://bit.ly/discord-collabcode"
+  },
+  {
+    command: "telegram",
+    response: "http://bit.ly/telegram-collabcode"
   },
   {
     command: "twitter",
